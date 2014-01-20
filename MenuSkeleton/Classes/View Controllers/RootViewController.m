343e6f74f5ce71e263a2abc9a4ccf318a15e96dd
Kevin Renskers <info@mixedcase.nl>
2014-01-20 12:54:57 +0000

    Seperate menu files for iPhone/iPad. Support modals with a specific size on iPad.

--- MenuSkeleton/Classes/View Controllers/RootViewController.m
+++ MenuSkeleton/Classes/View Controllers/RootViewController.m
@@ -21,7 +21,8 @@
 
 - (NSArray *)menu {
     if (!_menu) {
-        NSString *path = [[NSBundle mainBundle] pathForResource:@"menu" ofType:@"json"];
+        NSString *menuname = IsPhone ? @"menu-iPhone" : @"menu-iPad";
+        NSString *path = [[NSBundle mainBundle] pathForResource:menuname ofType:@"json"];
         NSData *data = [NSData dataWithContentsOfFile:path];
         _menu = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
     }
@@ -39,6 +40,14 @@
     [self switchToControllerAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
 }
 
+- (UIViewController *)controllerFromMenuItem:(NSDictionary *)menuItem {
+    if (menuItem[@"storyboard-identifier"]) {
+        return [UIViewController controllerFromStoryboard:menuItem[@"storyboard"] withIdentifier:menuItem[@"storyboard-identifier"]];
+    } else {
+        return [UIViewController initialControllerFromStoryboard:menuItem[@"storyboard"]];
+    }
+}
+
 #pragma mark - Public methods
 
 - (void)switchToControllerAtIndexPath:(NSIndexPath *)indexPath {
@@ -49,15 +58,25 @@
     if (menuItem[@"persistent"]) {
         controller = self.persistentControllers[indexPath];
         if (!controller) {
-            controller = [UIViewController initialControllerFromStoryboard:menuItem[@"storyboard"]];
+            controller = [self controllerFromMenuItem:menuItem];
             self.persistentControllers[indexPath] = controller;
         }
     } else {
-        controller = [UIViewController initialControllerFromStoryboard:menuItem[@"storyboard"]];
+        controller = [self controllerFromMenuItem:menuItem];
     }
 
     if (menuItem[@"modal"]) {
+        if (!IsPhone && menuItem[@"modal-size"]) {
+            controller.modalPresentationStyle = UIModalPresentationFormSheet;
+        }
+
         [self presentViewController:controller animated:YES completion:nil];
+
+        // Change the size of the modal if supplied
+        if (!IsPhone && menuItem[@"modal-size"]) {
+            NSArray *size = menuItem[@"modal-size"];
+            controller.view.superview.bounds = CGRectMake(0.0, 0.0, [size[0] integerValue], [size[0] integerValue]);
+        }
     } else {
         self.currentIndexPath = indexPath;
         self.contentViewController = controller;
