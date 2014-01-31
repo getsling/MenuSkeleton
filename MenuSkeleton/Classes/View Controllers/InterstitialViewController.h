320571d1f00c9c91a640bd329cc58430b220c965
Kevin Renskers <info@mixedcase.nl>
2014-01-31 14:49:34 +0000

    Obey the code style

--- MenuSkeleton/Classes/View Controllers/InterstitialViewController.h
+++ MenuSkeleton/Classes/View Controllers/InterstitialViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
+
 @interface InterstitialViewController : UIViewController
 
 @end
--- MenuSkeleton/Classes/View Controllers/RootViewController.h
+++ MenuSkeleton/Classes/View Controllers/RootViewController.h
@@ -8,6 +8,7 @@
 
 #import <REFrostedViewController/REFrostedViewController.h>
 
+
 @interface RootViewController : REFrostedViewController
 
 @property (readonly, nonatomic) NSArray *menu;
